0a7d4bbab7008623a08e4dc3dbd49ac2c9cf414f
rupinder jit kaur <cheemarupin89@gmail.com>
2022-11-22 15:59:10 +0000

    AddStudent.js edited to validate the student form to add new student

--- src/component/AddStudent.js
+++ src/component/AddStudent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import studentService from '../services/studentService';
 
 const AddStudent = () => {
@@ -7,27 +7,71 @@ const AddStudent = () => {
         id: "",
         firstName: "",
         lastName: "",
-        email: ""
+        email: "",
+        phone:'',
+        address:""
 
     });
+    const [formValues, setFormValues] = useState(student);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
     const handleChange = (e) =>{
         const value = e.target.value;
-        setStudent({ ...student, [e.target.name]: value});
+        setFormValues({ ...formValues, [e.target.name]: value});
     };
+
+    useEffect(() => {
+        console.log(formErrors);
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+          console.log(formValues);
+        }
+      }, [formErrors]);
+      const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    
+        if (!values.firstName) {
+          errors.firstName = "FirstName is required!";
+        }
+        if (!values.lastName) {
+            errors.lastName = "LastName is required!";
+          }
+        if (!values.email) {
+          errors.email = "Email is required!";
+        } else if (!regex.test(values.email)) {
+          errors.email = "This is not a valid email format!";
+        }
+        if (!values.phone) {
+          errors.phone = "Phone is required";
+        } else if (values.phone.length < 4) {
+          errors.phone = "phone must be more than 4 characters";
+        } else if (values.phone.length > 10) {
+          errors.phone = "phone cannot exceed more than 10 characters";
+        }
+        if (!values.address) {
+            errors.address = "Address is required!";
+          }
+        return errors;
+      };
+    
+
+    
 
     const saveStudent = (e) => {
         
-        studentService.saveStudent(student)
-        .then((response) => {
-            console.log(response)
+        e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+    };
 
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
+
+    const resetInputField = () => {
+      setFormValues("");
+    };
 
   return (
+    <form >
     <div className="flex max-w-2xl shadow mx-auto border-b">
                 <div className="px-8 py-8">
                     <div className="font-thin text-2xl tracking-wider">
@@ -42,10 +86,11 @@ const AddStudent = () => {
                         <input 
                         type="text" 
                         name = "firstName"
-                        value = {student.firstName}
+                        value = {formValues.firstName}
                         onChange={(e) => handleChange(e)}
                         className='h-10 w-96 border mt-2 px-2 py-2'></input>
                     </div>
+                    <p>{formErrors.firstName}</p>
                     
                     <div className="items-center justify-center  h-14 w-full my-4">
 
@@ -54,10 +99,11 @@ const AddStudent = () => {
                         </label>
                         <input type="text"
                                name = "lastName"
-                               value={student.lastName} 
+                               value={formValues.lastName} 
                                onChange={(e) => handleChange(e)}
                         className='h-10 w-96 border mt-2 px-2 py-2'></input>
                     </div>
+                    <p>{formErrors.lastName}</p>
 
                     <div className="items-center justify-center  h-14 w-full my-4">
 
@@ -65,11 +111,12 @@ const AddStudent = () => {
                             Email
                         </label>
                         <input type="email" 
-                               name = "emailId"
-                               value={student.emailId}
+                               name = "email"
+                               value={formValues.email}
                                onChange={(e) => handleChange(e)}
                         className='h-10 w-96 border mt-2 px-2 py-2'></input>
                     </div>
+                    <p>{formErrors.email}</p>
                     <div className="items-center justify-center  h-14 w-full my-4">
 
                         <label className="block">
@@ -77,10 +124,11 @@ const AddStudent = () => {
                         </label>
                         <input type="phone" 
                                name = "phone"
-                               value={student.phone}
+                               value={formValues.phone}
                                onChange={(e) => handleChange(e)}
                         className='h-10 w-96 border mt-2 px-2 py-2'></input>
                     </div>
+                    <p>{formErrors.phone}</p>
                     <div className="items-center justify-center  h-14 w-full">
 
                         <label className="block">
@@ -88,10 +136,11 @@ const AddStudent = () => {
                         </label>
                         <input type="address" 
                                name = "address"
-                               value={student.address}
+                               value={formValues.address}
                                onChange={(e) => handleChange(e)}
                         className='h-10 w-96 border mt-2 px-2 py-2'></input>
                     </div>
+                    <p>{formErrors.address }</p>
 
                     <div className="items-center justify-center  h-14 w-full my-4 space-x-4 pt-4">
                         <button  
@@ -99,13 +148,14 @@ const AddStudent = () => {
                         className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>
                             SAVE
                         </button>
-                        <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>
+                        <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6' onClick={resetInputField}>
                             CLEAR
                         </button>
                     </div>
 
     </div>
     </div>
+    </form>
   )
 }
 
