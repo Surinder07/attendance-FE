64a974b82049563d5d1f0f94beabe0ef6892bd87
rupinder jit kaur <cheemarupin89@gmail.com>
2022-11-23 16:12:33 +0000

    App.js edited online with Bitbucket
    HERE APP FILE IS CHANGED FOR ADDING HOME PAGE AND BATCH PAGE SEPARATELY

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+Original file 
+
 
 import './App.css';
 import Navbar from './component/Navbar';
@@ -25,3 +27,96 @@ function App() {
 }
 
 export default App;
+
+
+
+CHANGED FILE
+
+
+import './App.css';
+import Navbar from './component/Navbar';
+import AddStudent from './component/AddStudent';
+import Batches from './component/Batches';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from './component/Home';
+
+function App() {
+  return (
+  <>
+  <BrowserRouter>
+  <div className="bg-gray-800">
+      <div className="h-16 flex items-center">
+      <p className="text-white font-bold">Student Attendance System</p>
+      </div>
+      <div classname = "nav" style={{
+                    display:"flex",
+                   background: 'bg-gray-800',
+                    padding: '0px 2px 0px 1400px',
+                    fontSize: '25px',
+                    position: "relative",                   
+                    alignItems: "left",
+                    justifyContent: "space-auto",
+                    maxWidth: "225px"
+
+                    }}>
+                      <li style={ {float: "right", display: "block", color: "black"}}>
+                    <div style={{ margin: '5px' }}>
+                        <NavLink to="/" style={({ isActive }) => ({ 
+                            color: isActive ? 'Darkgray' : 'white' })}>
+                            Home
+                        </NavLink>
+                    </div>
+                    </li>
+                     
+               
+               
+                    <li style={ {float: "right", display: "block", color: "black"}}>
+                    <div style={{ margin: '5px' }}>
+                        <NavLink to="/Batches" style={({ isActive }) => ({ 
+                            color: isActive ? 'Darkgray' : 'white' })}>
+                            Batches
+                        </NavLink>
+                        </div>
+                    </li>
+                 
+                    <li style={ {float: "right", display: "block", color: "black"}}>
+                    <div style={{ margin: '5px' }}>
+                        <NavLink to="/Instructors" style={({ isActive }) => ({ 
+                            color: isActive ? 'Darkgray' : 'white' })}>
+                            Instructors
+                        </NavLink>
+                        </div>
+                    </li>
+           
+                        <li style={ {float: "right", display: "block", color: "black"}}>
+                    <div style={{ margin: '5px' }}>
+                        <NavLink to="/services" style={({ isActive }) => ({ 
+                            color: isActive ? 'Darkgray' : 'white' })}>
+                            Services
+                        </NavLink>
+                        </div>
+                    </li>
+              
+      
+    </div> 
+    </div>
+    
+     <Navbar/>
+     <Routes>
+     <Route path="/Home" element={<Home/>}></Route>
+        <Route index element = {<Home/>}></Route>
+        
+        <Route path ="/Batches" element={<Batches/>}></Route>
+        <Route path = "/addStudent" element={<AddStudent/>}></Route>
+
+     </Routes>
+  
+     </BrowserRouter>
+
+  </>
+  );
+}
+
+export default App;
+
+
